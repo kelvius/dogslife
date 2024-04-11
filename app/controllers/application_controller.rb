80548d03b5eb3659275df1833fb1321e41b5913c
kelvius <valeriojohnkelvin@gmail.com>
2024-04-11 14:20:40 -0500

    Added address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   # Other methods...
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address, :province])
+  end
   private
 
   # Redirect to login page after logout
